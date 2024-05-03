42c8d5748bf614a54a3bb7bb726b9d05c54ece1a
Saban-Adhikari <101613059+Saban-Adhikari@users.noreply.github.com>
2024-05-03 13:30:34 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainPage from "./MainPage.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
